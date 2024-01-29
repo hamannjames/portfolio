580840095745ee46af60da1912205cb69d357ffd
James Hamann <hamannjames87@gmail.com>
2024-01-28 23:32:14 -0800

    gtag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,6 +30,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-KC1C1F9DDL"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-KC1C1F9DDL');
+	</script>
 </svelte:head>
 
 <div class="app overflow-x-hidden">
